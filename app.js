7e211e11f6748571363ad41e5cc43236260be870
D3adSec <bruno.emedeiros99@gmail.com>
2024-05-16 13:40:44 -0300

    projeto em sala 16/05

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-
 import express from 'express'
 import cors from 'cors'
+import sql from 'mssql'
 
+import { sqlConfig } from './database.js';
+const pool = new sql.ConnectionPool(sqlConfig)
+await pool.connect();
 const app = express()
+
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
+app.get('/agendamentos', async (req, res)=>{
+    let {recordset} = await pool.query`select * from Agendamento`
+    return res.status(200).json(recordset)
+})
+
 app.listen(3000,()=>{
-    console.log('API running!')
+    console.log('runnning!')
 })
